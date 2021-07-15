4ad7a9bf85561a8db874746adbd5124b17ecb095
C9149-Mr.Bean <sdtaslan14@gmail.com>
2021-07-15 06:13:28 +0300

    Password Reminder assignment to uploaded

---
+++ python_assignments.py
@@ -0,0 +1,7 @@
+name = input("Enter a name: ").strip()
+if name == "Mr.Bean":
+    print("Hello", name, "Your password is: 12345")
+else:
+    print("Hello", name, "See you later!")
+
+# [A[B[C[Dhttps://github.com/SdtAslan/python-assignments.git
