3da4c6c15baeae6d1baa1d99749bd71fa5bac5bf
Sdtaslan <sdtaslan14@gmail.com>
2021-08-06 00:13:23 +0300

    n'e kadar prime number changed

--- prime number n'e kadar.py
+++ prime number n'e kadar.py
@@ -1,17 +1,15 @@
 y = input("Enter a positive number")
 if y.isdigit():
     x = int(y)
-    liste = [2]
-    k = 3
-    while k < x + 1:
+    liste = []
+    for k in range(2,x +1):
         count = 0
-        for i in range(2, k):
+        for i in range(1, k + 1):
             if (k % i) == 0:
                 count += 1
-        if count < 1:
-            liste.append(k)
-        k +=1        
-    print(liste)
+        if count <= 2:
+            liste.append(k)      
+    print("prime numbers up to:",x, ">>", liste)
     
 else:
     print("It is an invalid entry. Don't use non-numeric, float, or negative values!")
